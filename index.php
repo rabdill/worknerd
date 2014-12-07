d120de716fc2ad01a26fe5566b5ab45d79d47145
Rich Abdill <richard.abdill@gmail.com>
2014-12-07 22:41:52 +0000

    Tentative first front page (no search)

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<h1>techjob.io</h1>
+
 <?php	
 //Connect to DB:
 date_default_timezone_set ('America/New_York');
@@ -9,7 +11,14 @@ $data=mysql_select_db("results");
 
 $query="SELECT url, company, title, salary FROM listings;";
 $data=mysql_query($query);
-$info=mysql_fetch_array($data);
+?>
+<table border=1>
+<thead><th>Job<th>Company<th>Salary<tbody>
+<?php
+while ($info=mysql_fetch_array($data)) {
+echo "<tr><td><a href='" . $info['url'];
+echo "'>" . $info['title'] . "</a><td>" . $info['company'] . "<td>" . $info['salary'];
+}
 
-echo "<br><a href='" . $info['url'];
-echo "'>" . $info['title'] . "</a> at " . $info['company'] . " (salary: |" . $info['salary'] . "|)";
+?>
+</table>
