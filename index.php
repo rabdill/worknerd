9c71f4bb3a63a24a96e43d4ab1af2ea0d8e4a587
Rich Abdill <richard.abdill@gmail.com>
2014-12-07 22:28:22 +0000

    Got first front page working

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php	
 //Connect to DB:
 date_default_timezone_set ('America/New_York');
-$data=mysql_connect("job.czxcq0gunx4h.us-east-1.rds.amazonaws.com","aws","12WQasXZ!");
+$data=mysql_connect("job.czxcq0gunx4h.us-east-1.rds.amazonaws.com","aws","8!dkasLDJA7a&Aj");
 $data=mysql_select_db("results");
 
 
@@ -12,4 +12,4 @@ $data=mysql_query($query);
 $info=mysql_fetch_array($data);
 
 echo "<br><a href='" . $info['url'];
-echo "'>" . $info['title'] . "</a> at " . $info['company'] . " (salary: |" . $info['salary') . "|)";
+echo "'>" . $info['title'] . "</a> at " . $info['company'] . " (salary: |" . $info['salary'] . "|)";
