55f81484105722d3041c1a408ac0e66f13129877
Rich Abdill <richard.abdill@gmail.com>
2014-12-08 00:29:35 +0000

    Added website logos next to listings

--- index.php
+++ index.php
@@ -9,14 +9,19 @@ $data=mysql_select_db("results");
 
 
 
-$query="SELECT url, company, title, salary FROM listings;";
+$query="SELECT url, company, title, salary FROM listings ORDER BY company DESC;";
 $data=mysql_query($query);
 ?>
 <table border=1>
 <thead><th>Job<th>Company<th>Salary<tbody>
 <?php
 while ($info=mysql_fetch_array($data)) {
-echo "<tr><td><a href='" . $info['url'] . "'>" . $info['title'] . "</a><td>" . $info['company'] . "<td>" . $info['salary'] . "\n";
+echo "<tr><td><a href='" . $info['url'] . "'>";
+if (strpos($info['url'], 'www.dice') >= 0 && strpos($info['url'], 'www.dice') !== false) echo "<img src='img/dice.jpg' height=10>";
+elseif (strpos($info['url'], 'jobs.github') >= 0) echo "<img src='img/github.png' height=10>";
+else echo "<img src='http://imgc.allpostersimages.com/images/P-473-488-90/74/7476/IB2Q100Z/posters/danger-fart-zone-humor-sign-poster.jpg'>";
+
+echo " " . $info['title'] . "</a><td>" . $info['company'] . "<td>" . $info['salary'] . "\n";
 }
 
 ?>
