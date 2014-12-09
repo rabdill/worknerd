70feadaf0b3c51baeadeab65b1f2a5fdc58b5537
Rich Abdill <richard.abdill@gmail.com>
2014-12-09 17:27:49 +0000

    Tags display now, detect when one is part of search

--- results.php
+++ results.php
@@ -6,7 +6,7 @@ date_default_timezone_set ('America/New_York');
 $data=mysql_connect("job.czxcq0gunx4h.us-east-1.rds.amazonaws.com","aws","8!dkasLDJA7a&Aj");
 $data=mysql_select_db("results");
 
-$query="SELECT L.url AS url, L.company AS company, L.title AS title, L.salary as salary, x.tech as tech
+$query="SELECT L.jobid AS jobid, L.url AS url, L.company AS company, L.title AS title, L.salary as salary, x.tech as tech
 FROM listings L
 LEFT JOIN tags t ON L.jobid=t.jobid
 LEFT JOIN techs x ON t.techid=x.techid";
@@ -18,15 +18,26 @@ echo $query;
 $data=mysql_query($query);
 ?>
 <table border=1>
-<thead><th>Job<th>Company<th>Salary<tbody>
+<thead><th>Job<th>Company<th>Salary<th>Tags</tbody>
 <?php
 while ($info=mysql_fetch_array($data)) {
-echo "<tr><td><a href='" . $info['url'] . "'>";
-if (strpos($info['url'], 'www.dice') >= 0 && strpos($info['url'], 'www.dice') !== false) echo "<img src='img/dice.jpg' height=10>";
-elseif (strpos($info['url'], 'jobs.github') >= 0) echo "<img src='img/github.png' height=10>";
-else echo "<img src='http://imgc.allpostersimages.com/images/P-473-488-90/74/7476/IB2Q100Z/posters/danger-fart-zone-humor-sign-poster.jpg'>";
+    echo "<tr><td><a href='" . $info['url'] . "'>";
+    if (strpos($info['url'], 'www.dice') >= 0 && strpos($info['url'], 'www.dice') !== false) echo "<img src='img/dice.jpg' height=10>";
+    elseif (strpos($info['url'], 'jobs.github') >= 0) echo "<img src='img/github.png' height=10>";
+    else echo "<img src='http://imgc.allpostersimages.com/images/P-473-488-90/74/7476/IB2Q100Z/posters/danger-fart-zone-humor-sign-poster.jpg'>";
 
-echo " " . $info['title'] . "</a><td>" . $info['company'] . "<td>" . $info['salary'] . "\n";
+    echo " " . $info['title'] . "</a><td>" . $info['company'] . "<td>" . $info['salary'];
+
+    echo "<td>";
+    $query1="SELECT x.tech AS tech, x.techid AS techid FROM techs x
+	LEFT JOIN tags t ON x.techid=t.techid
+	WHERE t.jobid='" . $info['jobid'] . "';";
+    #echo $query1;
+    $data1=mysql_query($query1);
+    while ($info1=mysql_fetch_array($data1)) {
+	if(in_array($info1['techid'], $_GET['tech'])) echo "<strong>" . $info1['tech'] . "</strong> ";
+	else echo $info1['tech'] . " ";
+    }
 }
 
 ?>
