86fce7daaa521d5c437546d461dd99aed1ef5009
Rich Abdill <richard.abdill@gmail.com>
2014-12-09 23:04:22 -0500

    Almost done spider

--- spider/weworkremotely.sh
+++ spider/weworkremotely.sh
@@ -23,3 +23,38 @@ declare -a companies
 let i=0
 while read line
 do
+	urls[$i]=`echo $line`
+	echo "URL IS |${urls[$i]}|"
+	i=$(( i+1 ))
+done < listings.txt
+
+#Hit up the job pages
+total=${#urls[*]}
+i=0
+while [[ $i -lt $total ]]
+do
+	#check if you already have the listing
+	listing_check=`mysql -s -r -N -h $dbendpoint -D results -u $dbuser -p$dbpassword <<EOF
+	SELECT COUNT(url) FROM listings WHERE url="${urls[$i]}";	
+EOF`
+	if [[ listing_check -gt 0 ]]; then
+	    echo "Already have that listing."
+	else
+	    #read the job listing into a file
+	    curl -L ${urls[$i]} > temp.txt
+
+	    #scrape title
+	    salaries[$i]=`cat temp.txt | grep baseSalary | sed 's/ *<span property="baseSalary" content="\([^"]*\)"><\/span>/\1/g'`
+
+	#escape the mysql special characters (and HTML)
+	urls[$i]=`echo ${urls[$i]} | sed 's/[)(%"\\]/\\&/g' | sed "s/[']/\\\&/g" | sed 's/</\&lt\;/g' | sed 's/>/\&gt\;/g';`
+	titles[$i]=`echo ${titles[$i]} | sed 's/[)(%"\\]/\\\&/g' | sed "s/[']/\\\&/g" | sed 's/</\&lt\;/g' | sed 's/>/\&gt\;/g';`
+	salaries[$i]=`echo ${salaries[$i]} | sed 's/[)(%"\\]/\\&/g' | sed "s/[']/\\\&/g" | sed 's/</\&lt;/g' | sed 's/>/\&gt\;/g';`
+	descriptions[$i]=`echo ${descriptions[$i]} | sed 's/[)(%"\\]/\\&/g' | sed "s/[']/\\\&/g" | sed 's/</\&lt\;/g' | sed 's/>/\&gt\;/g';`
+
+	SQL=`mysql -s -r -N -h $dbendpoint -D results -u $dbuser -p$dbpassword <<EOF
+	INSERT INTO unprocessed (url, company, title, salary, description) VALUES ('${urls[$i]}', '${companies[$i]}', '${titles[$i]}', '${salaries[$i]}', '${descriptions[$i]}')
+EOF`
+	fi	
+	i=$(( i+1 ))
+done
